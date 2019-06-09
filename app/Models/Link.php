5bd4bf121914e25165831ac27da788a1eae8ff56
KaniRobinson <me@kanirobinson.co.uk>
2019-06-09 19:24:52 +0100

    Created Models for visits and Links

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'links';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'token', 'url',
+    ];
+
+    /**
+     * Visit Has Many Relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function visits() : \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this
+            ->hasMany(
+                \App\Models\Visit::class,
+                'link_id'
+            );
+    }
 }
--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visit extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'visits';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'link_id', 'ip_address', 'country',
+        'refereer_url',
+    ];
+
+    /**
+     * Link Belongs To Relationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function link() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this
+            ->belongsTo(
+                \App\Models\Link::class,
+                'link_id'
+            );
+    }
 }
